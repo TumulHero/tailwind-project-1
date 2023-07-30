ca0f0a560f409f9e769274908e6d93c300aa8cfc
TumulHero <122159878+TumulHero@users.noreply.github.com>
2023-07-30 23:16:24 +0600

    complete

---
+++ script.js
@@ -0,0 +1,6 @@
+let mobile_icon = document.getElementById("mobile_icon");
+let mobile_menu = document.getElementById("main_menu");
+
+mobile_icon.addEventListener("click",function(){
+    main_menu.classList.toggle('hidden');
+})
